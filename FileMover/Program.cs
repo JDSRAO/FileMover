b27d6aab1c3103b8ad740e391da79bf2f95b630b
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2020-06-09 20:25:33 +0530

    Completed code

--- FileMover/Program.cs
+++ FileMover/Program.cs
@@ -5,24 +5,47 @@ namespace FileMover
 {
     class Program
     {
-        private static string scanDirectory = "";
-        private static string outputDirectory = "";
-        private static string[] extensions = new string[] { "m3u8" };
+        private static string scanDirectory = @"C:\Users\SrinivasRao\Downloads\Temp soft";
+        private static string outputDirectory = @"C:\Users\SrinivasRao\Downloads\Outputs";
+        private static string[] extensions = new string[] { ".m3u8" };
+        private static bool moveFolder = true;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting the file mover");
-
-            foreach (var extension in extensions)
+            try
             {
-                var filePaths = Directory.GetFiles(scanDirectory, extension);
-                foreach (var filePath in filePaths)
+                Console.WriteLine("Starting the file mover");
+
+                Console.WriteLine("Scanning files started");
+                foreach (var extension in extensions)
                 {
-                    var fileName = Path.GetFileName(filePath);
-                    var outputFilePath = Path.Combine(outputDirectory, fileName);
-                    File.Move(filePath, outputFilePath);
+                    var filePaths = Directory.GetFiles(scanDirectory, $"*{extension}*");
+                    foreach (var filePath in filePaths)
+                    {
+                        var fileName = Path.GetFileName(filePath);
+                        var outputFilePath = Path.Combine(outputDirectory, fileName);
+                        File.Move(filePath, outputFilePath);
+                        if (moveFolder)
+                        {
+                            var fileNameWithContents = $"{fileName}_contents";
+                            var sourceFolderPath = Path.Combine(scanDirectory, fileNameWithContents);
+                            var outputPath = Path.Combine(outputDirectory, fileNameWithContents);
+                            if (Directory.Exists(sourceFolderPath))
+                            {
+                                Directory.Move(sourceFolderPath, outputPath);
+                            }
+                        }
+                    }
                 }
+
+                Console.WriteLine("Completed moving files, press enter to exit");
+                Console.ReadLine();
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred in the program : {ex.Message}");
+            }
+            
         }
     }
 }
