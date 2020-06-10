0de5c5d4cfd50c359dfad63703a6f42431e161bd
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2020-06-10 20:28:09 +0530

    Added config options

--- FileMover/Model/Configuration.cs
+++ FileMover/Model/Configuration.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace FileMover.Model
@@ -10,5 +12,16 @@ namespace FileMover.Model
         public string OutputDirectory { get; set; }
         public string[] Extensions { get; set; }
         public bool OnlyLog { get; set; }
+
+        public Configuration()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "config.json");
+            var configText = File.ReadAllText(filePath);
+            var config = JsonConvert.DeserializeObject<Configuration>(configText);
+            ScanDirectory = config.ScanDirectory;
+            OutputDirectory = config.OutputDirectory;
+            Extensions = config.Extensions;
+            OnlyLog = config.OnlyLog;
+        }
     }
 }
