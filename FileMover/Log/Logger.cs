1ceb220ae436bd5cdc508c90d337143aa0717d28
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2020-06-10 19:37:23 +0530

    added logger file

---
+++ FileMover/Log/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileMover.Log
+{
+    public class Logger
+    {
+        public string FileName { get; }
+
+        public string FilePath { get; }
+
+        public Logger()
+        {
+            FileName = $"{Guid.NewGuid()}.log";
+            FilePath = Path.Combine(Directory.GetCurrentDirectory(),"logs", FileName);
+            Directory.CreateDirectory(Path.GetDirectoryName(FilePath));
+            File.Create(FilePath);
+        }
+
+        public void Log(string text)
+        {
+            text = $"{DateTime.Now} : {text}";
+            File.AppendAllText(FilePath, text, Encoding.UTF8);
+        }
+
+    }
+}
