421e7285c7dd3af3486d41cff6e94f40b43218fb
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2020-06-10 20:23:16 +0530

    Added config model file

---
+++ FileMover/Model/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileMover.Model
+{
+    public class Configuration
+    {
+        public string ScanDirectory { get; set; }
+        public string OutputDirectory { get; set; }
+        public string[] Extensions { get; set; }
+        public bool OnlyLog { get; set; }
+    }
+}
